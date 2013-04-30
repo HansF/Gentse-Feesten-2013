f56b75cf5fad6364ab46eba319ae8534fb24b647
Tim Leytens <leytens.tim@gmail.com>
2013-04-30 19:09:24 +0200

    Initial Commit

---
+++ Gentse Feesten 2013/GFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GFAppDelegate.h
+//  Gentse Feesten 2013
+//
+//  Created by Tim Leytens on 30/04/13.
+//  Copyright (c) 2013 Tim Leytens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Gentse Feesten 2013/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Gentse Feesten 2013
+//
+//  Created by Tim Leytens on 30/04/13.
+//  Copyright (c) 2013 Tim Leytens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GFAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GFAppDelegate class]));
+    }
+}
