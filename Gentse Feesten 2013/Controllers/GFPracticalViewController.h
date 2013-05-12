503cac3685ae22f12f630fee7d87d127f2dd1820
Tim Leytens <leytens.tim@gmail.com>
2013-05-12 13:05:46 +0200

    Practical view controller

---
+++ Gentse Feesten 2013/Controllers/GFPracticalViewController.h
@@ -0,0 +1,13 @@
+//
+//  GFPracticalViewController.h
+//  Gentse Feesten 2013
+//
+//  Created by Tim Leytens on 12/05/13.
+//  Copyright (c) 2013 Tim Leytens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GFPracticalViewController : UIViewController
+
+@end
---
+++ Gentse Feesten 2013/Controllers/GFPracticalViewController.m
@@ -0,0 +1,38 @@
+//
+//  GFPracticalViewController.m
+//  Gentse Feesten 2013
+//
+//  Created by Tim Leytens on 12/05/13.
+//  Copyright (c) 2013 Tim Leytens. All rights reserved.
+//
+
+#import "GFPracticalViewController.h"
+
+@interface GFPracticalViewController ()
+
+@end
+
+@implementation GFPracticalViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
