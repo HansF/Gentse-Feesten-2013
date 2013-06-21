50f371080211540cbabc564b610da7bd8fd46f9c
Tim Leytens <leytens.tim@gmail.com>
2013-06-21 22:55:08 +0200

    add short name for dates

--- Gentse Feesten 2013/Models/GFDates.m
+++ Gentse Feesten 2013/Models/GFDates.m
@@ -19,42 +19,52 @@
                             [[NSMutableDictionary alloc] initWithObjectsAndKeys:
                              @"1374271200", @"id",
                              NSLocalizedString(@"ZATERDAG_20_JULI", nil), @"name",
+                             NSLocalizedString(@"ZA_20_JULI", nil), @"shortName",
                              nil],
                             [[NSMutableDictionary alloc] initWithObjectsAndKeys:
                              @"1374357600", @"id",
                              NSLocalizedString(@"ZONDAG_21_JULI", nil), @"name",
+                             NSLocalizedString(@"ZO_21_JULI", nil), @"shortName",
                              nil],
                             [[NSMutableDictionary alloc] initWithObjectsAndKeys:
                              @"1374444000", @"id",
                              NSLocalizedString(@"MAANDAG_22_JULI", nil), @"name",
+                             NSLocalizedString(@"MA_22_JULI", nil), @"shortName",
                              nil],
                             [[NSMutableDictionary alloc] initWithObjectsAndKeys:
                              @"1374530400", @"id",
                              NSLocalizedString(@"DINSDAG_23_JULI", nil), @"name",
+                             NSLocalizedString(@"DI_23_JULI", nil), @"shortName",
                              nil],
                             [[NSMutableDictionary alloc] initWithObjectsAndKeys:
                              @"1374616800", @"id",
                              NSLocalizedString(@"WOENSDAG_24_JULI", nil), @"name",
+                             NSLocalizedString(@"WOE_24_JULI", nil), @"shortName",
                              nil],
                             [[NSMutableDictionary alloc] initWithObjectsAndKeys:
                              @"1374703200", @"id",
                              NSLocalizedString(@"DONDERDAG_25_JULI", nil), @"name",
+                             NSLocalizedString(@"DO_25_JULI", nil), @"shortName",
                              nil],
                             [[NSMutableDictionary alloc] initWithObjectsAndKeys:
                              @"1374789600", @"id",
                              NSLocalizedString(@"VRIJDAG_26_JULI", nil), @"name",
+                             NSLocalizedString(@"VR_26_JULI", nil), @"shortName",
                              nil],
                             [[NSMutableDictionary alloc] initWithObjectsAndKeys:
                              @"1374876000", @"id",
                              NSLocalizedString(@"ZATERDAG_27_JULI", nil), @"name",
+                             NSLocalizedString(@"ZA_27_JULI", nil), @"shortName",
                              nil],
                             [[NSMutableDictionary alloc] initWithObjectsAndKeys:
                              @"1374962400", @"id",
                              NSLocalizedString(@"ZONDAG_28_JULI", nil), @"name",
+                             NSLocalizedString(@"ZO_28_JULI", nil), @"shortName",
                              nil],
                             [[NSMutableDictionary alloc] initWithObjectsAndKeys:
                              @"1375048800", @"id",
                              NSLocalizedString(@"MAANDAG_29_JULI", nil), @"name",
+                             NSLocalizedString(@"MA_29_JULI", nil), @"shortName",
                              nil],
                             nil];
     }
